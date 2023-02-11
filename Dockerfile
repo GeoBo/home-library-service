55178452f79d33d2ffdd215ddcef7f3cc08bc92a
Geobo <bozhys@gmail.com>
2023-02-11 13:27:42 +0300

    feat: add env config package

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,12 @@ FROM node:18-alpine
 
 WORKDIR /app
 
-COPY --chown=node:node ./package.json ./package-lock.json ./
-# COPY ./package.json ./package-lock.json ./
+COPY ./package.json ./package-lock.json ./
 
 RUN npm ci
 
-COPY --chown=node:node . ./
-# COPY . ./
+COPY . ./
 
 EXPOSE 4000
-
-USER node
 
 CMD ["npm", "run", "nodemon"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
 import { FavsModule } from './favs/favs.module';
 import { DbModule } from './db/db.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { DbModule } from './db/db.module';
     AlbumModule,
     FavsModule,
     DbModule,
+    ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [AppController],
   providers: [AppService],
